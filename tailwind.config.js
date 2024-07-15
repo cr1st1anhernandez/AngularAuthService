8f99e3df0f526685e5e2a8dd235e51a230a05f58
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-07-14 23:20:23 -0600

    :sparkles: Configure theme daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { cupcake } = require("daisyui/src/theming/themes");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
@@ -5,4 +7,15 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          primary: "#f0f0f0",
+          error: "#e11d48",
+        },
+      },
+    ],
+  },
 };
