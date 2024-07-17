93d791021f01084a786738082b6922d8ddc948cd
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-07-16 21:48:01 -0600

    :lock: Add jwt security

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,78 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { LoginResponse, RegisterResponse } from '../../models/responses.model';
+import { UserService } from '../user/user.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly LOGIN_URL = 'http://localhost:8080/auth/login';
+  private readonly REGISTER_URL = 'http://localhost:8080/auth/register';
+  private authenticatedSubject = new BehaviorSubject<boolean>(
+    this.isLoggedIn()
+  );
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private userService: UserService
+  ) {}
+
+  login(name: string, password: string): Observable<void> {
+    return this.http
+      .post<LoginResponse>(this.LOGIN_URL, { name, password })
+      .pipe(
+        map(response => {
+          if (response.jwt) {
+            localStorage.setItem('token', response.jwt);
+            const loginTime = new Date().toISOString();
+            localStorage.setItem('loginTime', loginTime);
+            this.authenticatedSubject.next(true);
+          }
+        })
+      );
+  }
+
+  register(name: string, email: string, password: string): Observable<void> {
+    return this.http
+      .post<RegisterResponse>(this.REGISTER_URL, { name, email, password })
+      .pipe(
+        map(response => {
+          if (response.message === 'Email already register!') {
+            throw new Error(response.message);
+          }
+        })
+      );
+  }
+
+  logout(): void {
+    if (typeof window !== 'undefined') {
+      localStorage.removeItem('token');
+      localStorage.removeItem('loginTime');
+      this.authenticatedSubject.next(false);
+      this.router.navigate(['/auth/login']);
+    }
+  }
+
+  isAuthenticated(): Observable<boolean> {
+    return this.authenticatedSubject.asObservable();
+  }
+
+  getToken(): string | null {
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem('token');
+    }
+    return null;
+  }
+
+  isLoggedIn(): boolean {
+    if (typeof window !== 'undefined') {
+      return this.getToken() !== null;
+    }
+    return false;
+  }
+}
